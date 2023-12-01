ef49cc0f397ea1ddd7d8c30eef671fc212811c77
Christian Nikolov <christian.nikolov@outlook.com>
2023-12-01 10:38:34 +0100

    add api actions

--- saga-test/src/lib/api.ts
+++ saga-test/src/lib/api.ts
@@ -9,3 +9,22 @@ export interface Todo {
 
 export const getTodos = async (): Promise<Todo> =>
   fetch(BASE_URL).then((res) => res.json());
+
+export const createTodo = async (title: string): Promise<Todo> =>
+  fetch(BASE_URL, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ title }),
+  }).then((res) => res.json());
+
+export const updateTodo = async (todo: Todo): Promise<Todo> =>
+  fetch(`${BASE_URL}/${todo.id}`, {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(todo),
+  }).then((res) => res.json());
+
+export const deleteTodo = async (id: number): Promise<void> =>
+  fetch(`${BASE_URL}/${id}`, {
+    method: "DELETE",
+  }).then((res) => res.json());
